db761553f214cfafc572ce9678c6178784b3c7b5
jangkyehun <ghjang0311@gmail.com>
2015-08-25 17:52:17 +0900

    출력 수정

--- src/EditDistance.scala
+++ src/EditDistance.scala
@@ -4,9 +4,10 @@
 object EditDistance {
   def main(args: Array[String]) = {
     val dist = distance(args(0), args(1))
+    print(" distance : " + dist)
   }
 
-  def distance (str1:String, str2:String) = {
+  def distance (str1:String, str2:String) : Int = {
     print(str1 + " " + str2)
     val matrix = Array.ofDim[Int](str1.length + 1, str2.length + 1)
     for (i <- 0 to str1.length;
@@ -18,7 +19,8 @@ object EditDistance {
                                matrix(i - 1)(j - 1) + (if (str1(i - 1) == str2(j - 1)) 0 else 1),
                                matrix(i)(j - 1) + 1 )
     }
-    print(" distance : " + matrix(str1.length)(str2.length))
+    //print(" distance : " + matrix(str1.length)(str2.length))
+    return matrix(str1.length)(str2.length)
   }
 
   def min(num1: Int, num2: Int, num3: Int) : Int = {
